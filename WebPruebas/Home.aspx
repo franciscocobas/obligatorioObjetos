45f6e1b94769a5d0587653b8192de2992868e9bf
Moira <moira.lasserre@gmail.com>
2015-02-21 02:14:04 -0200

    Validaciones, ingreso pasajero

--- WebPruebas/Home.aspx
+++ WebPruebas/Home.aspx
@@ -5,20 +5,26 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <title></title>
+    <title>Inicio</title>
+    <link href="styles.css" rel="stylesheet" />
 </head>
 <body class=" ">
     <form id="form1" runat="server">
     <div>
-
-        <h1 style="font-family: 'Courier New', Courier, monospace; font-size: large; font-weight: normal; font-style: normal; color: #000080;">Haz tu reserva aquí</h1>
-        <asp:TextBox ID="txt_documento" runat="server" onblur="if(this.value=='')this.value=this.defaultValue;" value="Documento" onfocus="if(this.value==this.defaultValue)this.value=''"></asp:TextBox>
+        <h1 id="h1_titleHome" style="font-family: 'Courier New', Courier, monospace; font-size: large; font-weight: normal; font-style: normal; color: #000080;">Haz tu reserva aquí</h1>
         <br />
-        <asp:DropDownList ID="drp_Pais" runat="server"></asp:DropDownList> 
-
         <br />
+        <div id="div_1" >
+            <asp:Label CssClass="cssLabels" ID="lbl_doc" runat="server" Text="Documento"></asp:Label><asp:TextBox ID="txt_documento" runat="server"></asp:TextBox>
+        </div>
+        <br />
+        <div id="div_2">            
+            <asp:Label CssClass="cssLabels" ID="lbl_pais" runat="server" Text="País emisor"></asp:Label><asp:DropDownList ID="drp_Pais" runat="server"></asp:DropDownList>
+        </div>
+        <br />
+        <div id="div_errorMessageDiv" runat="server"></div>
+        <div id="div_3"><asp:Button ID="Submit" runat="server" Text="Hacer reserva" OnClick="Submit_Click" /></div>
 
-        <asp:Button ID="Submit" runat="server" Text="Hacer reserva" OnClick="Submit_Click" />
         <br />
     </div>
     </form>
--- WebPruebas/ModificarUsuario.aspx.designer.cs
+++ WebPruebas/ModificarUsuario.aspx.designer.cs
@@ -13,6 +13,15 @@ namespace WebPruebas {
     public partial class ModificarUsuario {
         
         /// <summary>
+        /// titulo control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlTitle titulo;
+        
+        /// <summary>
         /// form1 control.
         /// </summary>
         /// <remarks>
@@ -22,57 +31,201 @@ namespace WebPruebas {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
-        /// editTitle control.
+        /// h1_editTitle control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlGenericControl editTitle;
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl h1_editTitle;
         
         /// <summary>
-        /// inputTitle control.
+        /// h1_inputTitle control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlGenericControl inputTitle;
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl h1_inputTitle;
         
         /// <summary>
-        /// Nombre control.
+        /// lbl_doc control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Nombre;
+        protected global::System.Web.UI.WebControls.Label lbl_doc;
         
         /// <summary>
-        /// TextBox1 control.
+        /// txt_documento2 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+        protected global::System.Web.UI.WebControls.TextBox txt_documento2;
         
         /// <summary>
-        /// Dirección control.
+        /// lbl_pais control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Dirección;
+        protected global::System.Web.UI.WebControls.Label lbl_pais;
         
         /// <summary>
-        /// TextBox2 control.
+        /// drp_Pais2 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox2;
+        protected global::System.Web.UI.WebControls.DropDownList drp_Pais2;
+        
+        /// <summary>
+        /// lbl_nombre control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbl_nombre;
+        
+        /// <summary>
+        /// txt_nombre control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_nombre;
+        
+        /// <summary>
+        /// lbl_dirección1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbl_dirección1;
+        
+        /// <summary>
+        /// txt_dir1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_dir1;
+        
+        /// <summary>
+        /// lbl_dirección2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbl_dirección2;
+        
+        /// <summary>
+        /// txt_dir2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_dir2;
+        
+        /// <summary>
+        /// lbl_ciudad control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbl_ciudad;
+        
+        /// <summary>
+        /// txt_ciudad control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_ciudad;
+        
+        /// <summary>
+        /// lbl_dptoProv control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbl_dptoProv;
+        
+        /// <summary>
+        /// txt_dptoProv control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_dptoProv;
+        
+        /// <summary>
+        /// lbl_CP control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbl_CP;
+        
+        /// <summary>
+        /// txt_CP control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_CP;
+        
+        /// <summary>
+        /// lbl_paisResid control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbl_paisResid;
+        
+        /// <summary>
+        /// drp_paisResid control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList drp_paisResid;
+        
+        /// <summary>
+        /// btn_modifDato control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_modifDato;
+        
+        /// <summary>
+        /// btn_hacerReserva control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_hacerReserva;
     }
 }
