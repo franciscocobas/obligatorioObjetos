2dc70d3187a677d05c180da7de30ffbbaeedeaf8
Moira <moira.lasserre@gmail.com>
2015-03-01 21:13:15 -0200

    Arreglado panel control

--- WebPruebas/ListarReservasPasaj.aspx.designer.cs
+++ WebPruebas/ListarReservasPasaj.aspx.designer.cs
@@ -31,6 +31,15 @@ namespace WebPruebas {
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl titulo;
         
         /// <summary>
+        /// btn_volver control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_volver;
+        
+        /// <summary>
         /// GridView1 control.
         /// </summary>
         /// <remarks>
